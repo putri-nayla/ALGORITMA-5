d3bf281cc06b45505c3cc371ed2746068b31123c
putri-nayla <putri20820@gmail.com>
2024-09-17 09:32:00 +0700

    nilai default argumen

---
+++ 03 nilai default argumen.php
@@ -0,0 +1,8 @@
+<?Php
+// Tanpa argumen
+function nama_bulan($bulan, $tahun = 2000) {
+	echo $bulan . '' . $tahun;
+    print_r("<p><strong>by :putri nayla</strong>");
+}	
+nama_bulan('maret'); // Hasil maret 2000
+?>
