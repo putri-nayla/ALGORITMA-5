4eb558d9687c935d2c6e9113b69e1b67275152f1
putri-nayla <putri20820@gmail.com>
2024-09-18 12:46:54 +0700

    argumen default

---
+++ 14 argumen default .php
@@ -0,0 +1,8 @@
+f<?php
+    function salam($waktu = "pagi") {
+    echo "Selamat $waktu!";
+    print_r("<p><strong>by : putri nayla</strong>");
+}
+
+salam(); // Output: Selamat pagi!
+salam("sore"); // Output: Selamat sore!
