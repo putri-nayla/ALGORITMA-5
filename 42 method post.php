13768060e80e39e631558a8ce8f7d6eb86badd2d
putri-nayla <putri20820@gmail.com>
2024-09-19 09:04:42 +0700

    method post

---
+++ 42 method post.php
@@ -0,0 +1,23 @@
+<html lang="en">
+ 
+<head>
+    <title>Method POST</title>
+</head>
+ 
+<body>
+    <form action="" method="POST">
+        <input type="text" name="nama"><br />
+        <input type="number" name="umur"><br />
+        <input type="submit" name="submit" value="Sumbit">
+    </form>
+ 
+    <?php
+    if ($_POST) {
+        echo "Nama:putri " . $_POST["nama"];
+        echo "<br/>";
+        echo "Umur: 16" . $_POST["umur"];
+    }
+    ?>
+</body>
+ 
+</html>
