6866702c038a6976deb9e88d5e6cd9162d003b8b
putri-nayla <putri20820@gmail.com>
2024-09-19 09:53:26 +0700

    memulai html

---
+++ 52 method post.php
@@ -0,0 +1,6 @@
+<?php
+if(isset($_POST['username']) && isset($_POST['password'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  // Proses username dan password untuk autentikasi
+}
