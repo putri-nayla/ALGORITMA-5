2e21bf1c45f2943aed60bd4ca4c0df725b1dab70
putri-nayla <putri20820@gmail.com>
2024-09-19 09:25:16 +0700

    get form

---
+++ 46 get form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Formulir GET di PHP</title>
+</head>
+<body>
+
+<form action="proses.php" method="get">
+    Nama: <input type="text" name="nama:putri"><br>
+    Email: <input type="text" name="email: putrinayla2020@gmail.com"><br>
+    <input type="submit" value="Kirim">
+</form>
+
+</body>
+</html>
